74567b7c2523165291f92fea47463449a3ffb6d4
Muhammadkarim-98 <105283322+Muhammadkarim-98@users.noreply.github.com>
2023-05-14 15:01:04 +0000

    updated

--- fp.js
+++ fp.js
@@ -29,3 +29,60 @@
 // return {name: newName, year: oldName.year}
 // }
 // console.log(changeName(thesis1,'Yahya'))
+
+//ITERATION (RECURSION & ITERATION)
+// function sum(numbers){
+// let total = 0;
+// for(let i = 0; i < numbers.length; i++){
+//      total += numbers[i];
+// }
+// return total
+// }
+// console.log(sum([1,2,3,4]))
+
+//RECURSION - 1
+// function sum(numbers){
+// if(numbers.length === 1){
+//     // base case
+//     return numbers[0];
+// } else{
+//     // recursive case
+// return numbers[0] + sum(numbers.slice(1));
+// }
+// }
+// console.log(sum([1,2,3,4]))
+//RECURSION - 2
+// function recursiveFact(num){
+// if(num === 0) {
+//     //base case
+//     return 1
+// } else{
+//     //recursive case
+//     return num * recursiveFact(num - 1)
+// }
+// };
+// console.log(recursiveFact(5));
+
+// ITERATIVE IS NOT EASY😵
+// function fibonacciIterative(n) {
+//     if (n === 0) return 0;
+//     if (n === 1) return 1;
+//     let prev1 = 1;
+//     let prev2 = 0;
+//     let current;
+//     for (let i = 2; i <= n; i++) {
+//       current = prev1 + prev2;
+//       prev2 = prev1;
+//       prev1 = current;
+//     }
+//     return current;
+//   }
+//   console.log(fibonacciIterative(6));
+
+//RECURSIVE IS EASY 😎
+// function fibonacciRecursive(num){
+//     if (num === 0) return 0;
+//     if (num === 1) return 1;
+//     return fibonacciRecursive(num - 1) + fibonacciRecursive(num -2);
+// }
+// console.log(fibonacciRecursive(6))
